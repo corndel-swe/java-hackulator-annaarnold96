54cd0beb80773630df1aff888dc8a08b4f12e269
Anna Arnold <anna.arnold.96@gmail.com>
2024-09-17 15:05:57 +0100

    fixes maxofthree.java to deal with same numbers

--- src/main/java/com/corndel/hackulator/MaxOfThree.java
+++ src/main/java/com/corndel/hackulator/MaxOfThree.java
@@ -3,6 +3,41 @@ package com.corndel.hackulator;
 public class MaxOfThree {
 
   static int maxOfThree(int x, int y, int z) {
+    if (x == y && x == z) {
+      return x;
+    }
+
+    if (x != y && x == z) {
+      if (x > y){
+        return x;
+      }
+      else {
+        return y;
+      }
+    }
+
+    if (x == y && x != z) {
+      if (x > z){
+        return x;
+      }
+      else {
+        return z;
+      }
+    }
+
+    if (y == z && y != x) {
+      if (y > x){
+        return y;
+      }
+      else {
+        return x;
+      }
+    }
+
+    if (x == z && x < y) {
+      return x;
+    }
+
     if (x > y && x > z) {
       return x;
     }
